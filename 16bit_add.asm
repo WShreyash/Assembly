b94ce3b60b6056de7cbf5b8aa410f7bb219f26fe
kagenou <wale11shreyash@gmail.com>
2025-08-30 16:58:33 +0530

    16 bit addition using GDB

---
+++ 16bit_add.asm
@@ -0,0 +1,27 @@
+;addition of two 16bit numbers using gdb
+
+global _start
+
+section .data
+    num1 dw 1234
+    num2 dw 4321
+
+section .text
+_start:
+    mov ax,[num1]
+    add ax,[num2]
+
+    mov eax,1
+    xor ebx,ebx
+    int 80h
+
+
+;GDB cmds
+;nasm -f elf32 -g -F dwarf 16bit_add.asm -o 16bit_add.o
+;ld -m elf_i386 16bit_add.o -o 16bit_add
+;gdb ./16bit_add
+;break _start
+;run
+;nexti         
+;info registers
+
